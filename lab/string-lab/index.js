90b4645cc686feaa2027c3590bd2d6dbce42db76
cynthiawuor <cynthiawuor254@gmail.com>
2023-07-27 16:22:03 +0300

    arithmetic-lab

---
+++ lab/string-lab/index.js
@@ -0,0 +1,17 @@
+function solveEquation(numbers) 
+var multiply
+ num1 * num2
+    //=>62
+
+var random
+//=>0
+
+function setEquation(numbers)
+var mod 
+ num3/num4
+ Math.floor(num3/num4)
+ 
+var max
+math.max();
+
+console.log
